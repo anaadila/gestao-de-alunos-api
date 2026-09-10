7a94b10b34535cf1e2a6b0cc9d58c53a6838e171
Ana Ádila <anaadilacc@gmail.com>
2026-09-10 08:15:29 -0300

    test: adiciona testes adicionais de erro 400 e 401 da rota de login

--- test/login.test.js
+++ test/login.test.js
@@ -3,6 +3,7 @@ import app from '../src/app.js';
 import { expect } from 'chai';
 
 describe('Login', () => {
+
     it('deve retornar 200 quando o usuário e senha forem corretos', async () => {
         const loginResposta = await request(app)
             .post('/api/auth/login')
@@ -11,4 +12,41 @@ describe('Login', () => {
         
         expect(loginResposta.status).to.equal(200);
     });
+
+    it('deve retornar 400 quando o usuário não enviar email', async () => {
+        const loginResposta = await request(app)
+            .post('/api/auth/login')
+            .set('Content-Type', 'application/json')
+            .send({ 'senha': 'admin123' });
+        
+        expect(loginResposta.status).to.equal(400);
+    });
+
+    it('deve retornar 400 quando o usuário não enviar senha', async () => {
+        const loginResposta = await request(app)
+            .post('/api/auth/login')
+            .set('Content-Type', 'application/json')
+            .send({ 'email': 'admin@escola.com' });
+        
+        expect(loginResposta.status).to.equal(400);
+    });
+
+    it('deve retornar 401 quando o usuário inserir uma senha incorreta', async () => {
+        const loginResposta = await request(app)
+            .post('/api/auth/login')
+            .set('Content-Type', 'application/json')
+            .send({ 'email': 'admin@escola.com', 'senha': 'admin1234' });
+        
+        expect(loginResposta.status).to.equal(401);
+    });
+
+    it('deve retornar 401 quando o usuário for inexistente', async () => {
+        const loginResposta = await request(app)
+            .post('/api/auth/login')
+            .set('Content-Type', 'application/json')
+            .send({ 'email': 'inexistente@inexistente.com', 'senha': 'admin123' });
+        
+        expect(loginResposta.status).to.equal(401);
+    });
+
 });
